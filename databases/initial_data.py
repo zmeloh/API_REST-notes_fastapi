5d74f1f8ce67881f0319d0cadf6c098a83f07f83
Esmer Laurent <esmerlaurent@gmail.com>
2024-03-05 22:17:42 -0500

    improve

--- databases/initial_data.py
+++ databases/initial_data.py
@@ -1,5 +1,5 @@
 import asyncio
-from tortoise import Tortoise
+from tortoise import Tortoise, exceptions
 from models.user import User
 
 async def add_initial_data():
@@ -11,13 +11,31 @@ async def add_initial_data():
     ]
 
     for user_data in users:
-        await User.create(**user_data)
+        try:
+            await User.create(**user_data)
+            print(f"User {user_data['username']} added.")
+        except exceptions.IntegrityError:
+            print(f"User {user_data['username']} already exists.")
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(Tortoise.init(
-        db_url='sqlite://my_notes.db',
-        modules={'models': ['models.user', 'models.note']}
-    ))
-    loop.run_until_complete(add_initial_data())
-    loop.run_until_complete(Tortoise.close_connections())
+    try:
+        await Tortoise.init(
+            db_url='sqlite://my_notes.db',
+            modules={'models': ['models.user', 'models.note']}
+        )
+        await add_initial_data()
+    finally:
+        await Tortoise.close_connections()
+
+
+import asyncio
+from tortoise import Tortoise, exceptions
+from models.user import User
+
+async def add_initial_data():
+    # Créer des utilisateurs de test
+    users = [
+        {"username": "utilisateur1", "password": "motdepasse1"},
+        {"username": "utilisateur2", "password": "motdepasse2"},
+        {"username": "utilisateur
