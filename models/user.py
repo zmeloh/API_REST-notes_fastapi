d7356ac6f170927be5ce367febe62e58a242acc1
Esmer Laurent <esmerlaurent@gmail.com>
2024-03-05 22:17:39 -0500

    improve

--- models/user.py
+++ models/user.py
@@ -1,19 +1,7 @@
-from tortoise import fields, models
-from pydantic import BaseModel
+import bcrypt
 from typing import List
 
-class User(models.Model):
-    id = fields.IntField(pk=True)
-    username = fields.CharField(max_length=255, unique=True)
-    password = fields.CharField(max_length=255)  # Vous devriez utiliser un stockage sécurisé des mots de passe en production
+import tortoise.contrib.pydantic as model_fields
+from pydantic import BaseModel
+from tortoise import fields, models
 
-    def __str__(self):
-        return self.username
-    
-class UserIn(BaseModel):
-    username: str
-    password: str
-
-class UserOut(BaseModel):
-    id: int
-    username: str
